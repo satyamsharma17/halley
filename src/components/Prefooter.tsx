1e08f0683f873b6349c1192258a63355e0c81c46
yashketanagnihotri <78458048+yashketanagnihotri@users.noreply.github.com>
2023-12-01 18:00:57 +0530

    Update Prefooter.tsx

--- src/components/Prefooter.tsx
+++ src/components/Prefooter.tsx
@@ -5,14 +5,14 @@ export default function Prefooter() {
     <div className="bg-gray-50 dark:bg-neutral-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 pt-20 pb-10">
         <h1 className="text-4xl font-bold text-center">
-          ¿Quieres una copia del código funete?
+          Elevating Travel with Seamless Assistance.
         </h1>
       </div>
 
       <div className="text-center pb-20">
-        <Link href="https://github.com/humberni/halley" target="_blank">
+        <Link href="/" target="_blank">
           <button className="bg-rose-500 text-white rounded-full px-6 p-3 font-medium">
-            Obtener proyecto
+            Go To Top
           </button>
         </Link>
       </div>
